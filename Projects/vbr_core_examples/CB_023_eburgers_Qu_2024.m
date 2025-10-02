18ebd8ec55b50a6489f5078f4bfea207e2f5fe5c
chavlin <chris.havlin@gmail.com>
2025-10-02 12:03:40 -0500

    temp branch for validation

--- Projects/vbr_core_examples/CB_023_eburgers_Qu_2024.m
+++ Projects/vbr_core_examples/CB_023_eburgers_Qu_2024.m
@@ -38,6 +38,8 @@ function VBR = CB_023_eburgers_Qu_2024()
   [VBR] = VBR_spine(VBR) ;
 
   %% build figure %%
+  Tmin = 800;
+  Tmax = 1200;
   if ~vbr_tests_are_running()
     figure('PaperPosition',[0,0,8.5,4],'PaperPositionMode','manual');
     for iTemp = 1:numel(VBR.in.SV.T_K)
@@ -46,8 +48,9 @@ function VBR = CB_023_eburgers_Qu_2024()
       Q_out=squeeze(VBR.out.anelastic.eburgers_psp.Qinv(1,iTemp,:));
 
       logper=log10(1./VBR.in.SV.f);
-      R=(iTemp-1) / (numel(VBR.in.SV.T_K)-1);
-      B=1 - (iTemp-1) / (numel(VBR.in.SV.T_K)-1);
+      T_i = VBR.in.SV.T_K(iTemp) - 273;
+      R=(T_i - Tmin) / (Tmax - Tmin);
+      B=1 - R;
       T_i = VBR.in.SV.T_K(iTemp);
       lab = [num2str(T_i-273), ' C'];
       subplot(1,2,1)
@@ -61,13 +64,41 @@ function VBR = CB_023_eburgers_Qu_2024()
     subplot(1,2,1)
     title("Qu et al 2024", 'Interpreter', 'none')
     ylabel('M [GPa] ')
-    ylim([20,80])
+    % ylim([20,80])
 
     subplot(1,2,2)
     ylabel('Q^-1');
-    ylim([-2.5,0.5])
-    legend('location', 'northwest', 'NumColumns', 2)
+    % ylim([-2.5,0.5])
+    % legend('location', 'northwest', 'NumColumns', 2)
 
+    if exist('Qu_2024_data.mat')
+        Qu = load('Qu_2024_data.mat');
+        for isamp = 1:numel(Qu.samples)
+            samp = Qu.samples{isamp};
+            data = Qu.(samp);
+            disp(fieldnames(data))
+            T_K = data.T_K;
+            for iT = 1:numel(T_K);
+
+                T_i = T_K(iT) - 273;
+                R=(T_i - Tmin) / (Tmax - Tmin);
+                if R<0
+                    R = 0;
+                elseif R > 1
+                    R = 1;
+                end
+                B=1 - R;
+
+                G = data.G_GPa(iT);
+                logQs_inv = data.logQs_inv(iT);
+                logper = data.log_period(iT);
+                subplot(1,2,1)
+                plot(logper, G, 'linestyle', 'none', 'marker', '.', 'markersize', 4, 'color', [R, 0, B])
+                subplot(1,2,2)
+                plot(logper, logQs_inv, 'linestyle', 'none', 'marker', '.', 'markersize', 4, 'color', [R, 0, B])
+            end
+        end
+    end
     for ip = 1:2
       subplot(1,2,ip); box on;
       xlabel('period [s]')
